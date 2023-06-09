fedba34f059ca19b39e9cb0de25fe155df3fbcdd
Pedro Gonzalez <viento100@gmail.com>
2023-06-09 12:57:58 -0400

    Creacion de myApp

---
+++ myApp.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+function home(req, res) {
+  var absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+}
+
+function jason(req, res) {
+  let respuesta;
+  const mySecret = process.env["MESSAGE_STYLE"];
+  if (mySecret === "uppercase") {
+    respuesta = "Hello Json".toUpperCase();
+  } else {
+    respuesta = "Hello Json";
+  }
+  res.json({ message: respuesta });
+}
+function jsonJuan(req, res) {
+  res.json({
+    message: "Hello json",
+  });
+}
+
+app.get("/json", jason);
+app.get("/", home);
